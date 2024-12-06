1983af1ed2d6c96f669a866bd1d9ff8bcb96f6d0
whoisfpc <6935891+whoisfpc@users.noreply.github.com>
2024-12-07 00:06:49 +0800

    update bat

--- run.bat
+++ run.bat
@@ -1,1 +1,18 @@
+@echo off
+if [%3]==[] goto debug
+
+if [%3]==[-speed] goto speed
+
+:debug
+@echo on
 odin run src -debug -sanitize:address  -- %1 %2
+@echo off
+goto :done
+
+:speed
+@echo on
+odin run src -o:speed  -- %1 %2
+@echo off
+goto :done
+
+:done
